237823fec575b7bbc7e4c29c2f5d978ec858ffc0
piumal1999 <piumalrathnayake@hotmail.com>
2021-10-02 12:26:21 +0530

    Handle input arguments

--- visualizer.c
+++ visualizer.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#define MEETINGS 0
+#define TIME 1
+#define PARTICIPANTS 2
+
 
 struct _ {
     int occurrences;
@@ -11,6 +17,9 @@ struct _ {
 } typedef meetingHost_t;
 
 meetingHost_t *meetingHosts = NULL;
+int mode;
+int isScaled = 0;
+int rows;
 
 int main(int argc, char ** argv) {
     char * fileNames[255];
@@ -20,19 +29,42 @@ int main(int argc, char ** argv) {
     int argIndex = 1;
     while (argIndex < argc) {
         if (0 == strcmp(argv[argIndex], "-m")) {
-            // change mode
+            mode = MEETINGS;
         } else if (0 == strcmp(argv[argIndex], "-t")) {
-            // change mode
+            mode = TIME;
         } else if (0 == strcmp(argv[argIndex], "-p")) {
-            // change mode
+            mode = PARTICIPANTS;
         } else if (0 == strcmp(argv[argIndex], "-l")) {
-            // set graph height
+            if (argIndex == argc-1) {
+                // print length not given error
+                return 0;
+            } else {
+                char * lString = argv[argIndex+1];
+                for (int i = 0; lString[i]!='\0' ; i++) {
+                    if (isdigit(lString[i]) == 0 && !(i==0 && lString[1]!='\0' && (lString[i] == '+' || lString[i] == '-'))){
+                        // print invalid options for length error
+                        return 0;
+                    }
+                }
+                int length = atoi(lString);
+                if (length<0){
+                    // print minus length error
+                } else {
+                    rows = length;
+                }
+            }
         } else if (0 == strcmp(argv[argIndex], "--scaled")) {
-            // set scaled mode on
+            isScaled = 1;
         } else {
             // terminate immediately if not .csv format
-            filenames[filenameIndex] = argv[argIndex];
-            filenameIndex++;
+            int length = strlen(argv[argIndex]);
+            char *extension = &(argv[argIndex])[length-4];
+            if(0 != strcmp(extension, ".csv")) {
+                // print incorrect file message
+                return 0;
+            }
+            fileNames[fileNamesIndex] = argv[argIndex];
+            fileNamesIndex++;
         }
         argIndex++;
     }
